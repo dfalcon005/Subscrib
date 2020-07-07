2c3d6b33c095056663f1c74090a49eabd3811003
dfalcon005 <dfalcon005@gmail.com>
2020-07-07 18:33:37 -0500

    Created Subscription list and Main Page Layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Mainpage from './components/MainPage/MainPage'
+
 function App() {
   return (
     <Router>
       <h2>Navbar</h2>
       <div className="container">
-        <h2>MainPage</h2>
-        <h2>Details Page</h2>
+        <Mainpage/>
       </div>
     </Router>
   );
---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MainPage.css'
+import Subscription from './Subscription/Subscription';
+import AddButton from '../Buttons/AddButton';
+
+const mainpage = (props) => (
+    <div className="row">
+        {/* subscription list */}
+        <div className="col-4">
+            <h3>My Subscriptions:</h3>
+            <Subscription/>
+            <AddButton/>
+        </div>
+        {/* line chart */}
+        <div className="col-8">
+            <h3>You are currently spending <span className="informative-label">$9.99/month</span>...</h3>
+            {/* place line chart here */}
+            <h1>Line chart goes here</h1>
+        </div>
+    </div>
+);
+
+export default mainpage;
