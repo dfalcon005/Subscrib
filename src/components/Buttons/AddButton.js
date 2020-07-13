f4f1e92d55f4fd3613b8e3539cc7603ec0e177d9
dfalcon005 <dfalcon005@gmail.com>
2020-07-13 15:27:12 -0500

    Changed button color to match navbar

--- src/components/Buttons/AddButton.js
+++ src/components/Buttons/AddButton.js
@@ -3,7 +3,7 @@ import './AddButton.css';
 
 const addButton = () => (
     <div className="button-position">
-        <button className="btn circle-btn float-right">
+        <button className="btn-dark circle-btn float-right">
             {/* + sign object */}
             <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
