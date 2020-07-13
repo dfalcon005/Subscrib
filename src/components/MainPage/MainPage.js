993e3cf0a7c76188cbc41dd9b9e388926e4e43b7
dfalcon005 <dfalcon005@gmail.com>
2020-07-13 15:23:12 -0500

    Changed MainPage.js to class based component to put the subscription list in a state variable

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './MainPage.css';
 import Subscription from './Subscription/Subscription';
 import AddButton from '../Buttons/AddButton';
 import SimpleLineChart from './LineChart/SimpleLineChart';
 
 
-const mainpage = (props) => (
-    <div className="row">
-        {/* subscription list */}
-        <div className="col-sm-4">
-            <h3>My Subscriptions:</h3>
-            <Subscription/>
-            <AddButton/>
-        </div>
-        {/* line chart */}
-        <div class="col-sm-8" id = "linechart">
-            <div class="card">
-            <div class="card-body">
-                <h3 class="card-title">You are currently spending <span className="informative-label">$9.99/month</span>...</h3>
-                    {/* place line chart here */}
-                <div><SimpleLineChart/></div>
+class mainpage extends Component{
+    state = {
+        subscriptions: [
+            { id: '1', name: 'Netflix', price: '$9.99/month', next_pay: '7/21'},
+            { id: '2', name: 'Spotify', price: '$5.99/month', next_pay: '7/25'},
+            { id: '3', name: 'Hulu', price: '$5.99/month', next_pay: '7/29'},
+            { id: '4', name: 'Disney+', price: '$9.99/month', next_pay: '7/31'},
+            { id: '5', name: 'Gym', price: '$35.99/month', next_pay: '8/21'},
+          ],
+    }
+
+    render(){
+        return(
+            <div className="row">
+                {/* subscription list */}
+                <div className="col-sm-4" id="sub-list-section">
+                    <h4>My Subscriptions:</h4>
+                    {/* maps subscriptions to create a card for each */}
+                    {this.state.subscriptions.map((sub, index) => {
+                        return <Subscription
+                            name={sub.name}
+                            price={sub.price}
+                            next_pay={sub.next_pay}/>
+                    })}
+                    <AddButton/>
                 </div>
+                {/* line chart */}
+                <div class="col-sm-8" id = "linechart">
+                    <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">You are currently spending <span className="informative-label">$9.99/month</span>...</h3>
+                            {/* place line chart here */}
+                        <div><SimpleLineChart/></div>
+                        </div>
+                    </div>
+                    </div>
             </div>
-            </div>
-    </div>
-);
+        )
+    }
+};
 
 export default mainpage;
--- src/components/MainPage/Subscription/Subscription.js
+++ src/components/MainPage/Subscription/Subscription.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import './Subscription.css'
 
-const subs = ['Netflix', 'Hulu', 'Gym', 'Spotify']
-
 const subscription = (props) => (
     <div>
-        {/* Loop through all subscriptions */}
-        {subs.map((sub, index) => (
-            <div className="subscription" key={index}>
-                {/* card for subscription */}
-                <div className="card">
+        <div className="subscription">
+            {/* card for subscription */}
+            <div className="card">
 
-                    {/* div for top items */}
-                    <div className="d-flex justify-content-between">
-                        <h5>{sub}</h5>
-                        <p className="informative-label">$9.99/month</p>
-                    </div>
-                    {/* next payment info */}
-                    <p className="descriptive-label">Next payment:</p>
-                    <p className="informative-label">7/21</p>
+                {/* div for top items */}
+                <div className="d-flex justify-content-between">
+                    <h5>{props.name}</h5>
+                    <p className="informative-label">{props.price}</p>
                 </div>
+                {/* next payment info */}
+                <p className="descriptive-label">Next payment:</p>
+                <p className="informative-label">{props.next_pay}</p>
             </div>
-        ))}
+        </div>
     </div>
 );
 
