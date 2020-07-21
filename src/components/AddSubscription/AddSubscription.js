5c4053b3a14af62d2c2985d76aeaf0a28a1b9f51
dfalcon005 <dfalcon005@gmail.com>
2020-07-21 17:41:42 -0500

    finished add subscription feature

--- src/components/AddSubscription/AddSubscription.js
+++ src/components/AddSubscription/AddSubscription.js
@@ -1,84 +1,116 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
-import DatePicker from 'react-datepicker';
-import CurrencyInput from 'react-currency-input-field';
-import "react-datepicker/dist/react-datepicker.css";
 import './AddSubscription.css';
 import Button from '../Buttons/Button';
 
 class addSubscription extends Component{
     state = {
-        startDate: new Date()
+        startDate: new Date(),
+        name: '',
+        category: '',
+        sub_payment: 0,
+        date_purchased: Date.now(),
+        payment_freq: '',
+        sub_type: '',
+        trial_ending: Date.now(),
     };
-     
-    handleChange = date => {
-        this.setState({
-            startDate: date
-        });
-    };
+
+    // handler for form submission
+    subscriptionDataHandler = (event) => {
+        event.preventDefault();
+        
+        const subscription = {
+            name: this.state.name,
+            category: this.state.category,
+            sub_payment: this.state.sub_payment,
+            date_purchased: this.state.date_purchased,
+            payment_freq: this.state.payment_freq,
+            sub_type: this.state.sub_type,
+            trial_ending: this.state.trial_ending
+        }
+
+        axios.post('http://localhost:4000/Subscription/add', subscription)
+            .then(response => {
+                console.log(response)
+                this.props.history.push('/');
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }
 
     render(){
         return (
             <div className="newSub-form">
-                <form>
+                <form onSubmit={this.subscriptionDataHandler}>
                     <div className="form-group">
                         <h5>Name</h5>
-                        <input type="text" className="form-control" placeholder="Subscription Name"/>
+                        <input required value={this.state.name} onChange={(event) => this.setState({name: event.target.value})} type="text" className="form-control" placeholder="Subscription Name"/>
                     </div>
                     <div className="form-group">
                         <h5>Category</h5>
-                        <select className="custom-select mr-sm-2">
-                            <option>Music</option>
-                            <option>Phone Services</option>
-                            <option>Shopping</option>
-                            <option>Streaming</option>
+                        <select required 
+                            value={this.state.category} 
+                            onChange={async (event) => await this.setState({category: event.target.value})}
+                            className="custom-select mr-sm-2">
+                            <option selected>Category</option>
+                            <option value="Music">Music</option>
+                            <option value="Shopping">Shopping</option>
+                            <option value="Streaming">Streaming</option>
+                            <option value="Phone Services">Phone Services</option>
                         </select>
                     </div>
                     <div className="form-group">
                         <h5>Payment amount</h5>
-                        <CurrencyInput className="custom-input"
-                            id="input-example"
-                            name="input-name"
-                            placeholder="$1,000"
-                            prefix="$"
-                            allowDecimals={true}
-                            decimalsLimit={2}
-                            onChange={(value, name) => console.log(value, name)}
+                        <input required type="number"
+                            step="0.01"
+                            className="custom-input"
+                            onChange={(event) => this.setState({sub_payment: event.target.value})}
                         />
                     </div>
                     <div className="form-group">
                         <h5>Date Purchased</h5>
-                        <DatePicker className="custom-input"
-                        selected={this.state.startDate}
-                        onChange={this.handleChange}/>
+                        <input required type="date" 
+                            className="custom-input"
+                            onChange={(event) => this.setState({date_purchased: event.target.value})}
+                        />
                     </div>
                     <div className="form-group">
                         <h5>Payment Frequency</h5>
-                        <select className="custom-select mr-sm-2">
-                            <option>Weekly</option>
-                            <option>Monthly</option>
-                            <option>Yearly</option>
+                        <select required 
+                            value={this.state.payment_freq} 
+                            onChange={async (event) => await this.setState({payment_freq: event.target.value})}
+                            className="custom-select mr-sm-2">
+                            <option selected>Payment Frequency</option>
+                            <option value="Weekly">Weekly</option>
+                            <option value="Monthly">Monthly</option>
+                            <option value="Yearly">Yearly</option>
                         </select>
                     </div>
                     <div className="form-group">
                         <h5>Subscription Type</h5>
-                        <select className="custom-select mr-sm-2" id="sub_type" name="sub_type">
-                            <option>Standard</option>
-                            <option>Premium</option>
-                            <option>Family</option>
-                            <option>Trial</option>
+                        <select required 
+                            value={this.state.sub_type} 
+                            onChange={async (event) => await this.setState({sub_type: event.target.value})}
+                            className="custom-select mr-sm-2">
+                            <option selected>Subscription Type</option>
+                            <option value="Standard">Standard</option>
+                            <option value="Premium">Premium</option>
+                            <option value="Family">Family</option>
+                            <option value="Trial">Trial</option>
                         </select>
                     </div>
                     <div className="form-group">
                         <h5>Trial Ending</h5>
-                        <DatePicker className="custom-input"
-                        selected={this.state.startDate}
-                        onChange={this.handleChange}/>
+                        <input type="date" 
+                            className="custom-input"
+                            onChange={(event) => this.setState({trial_ending: event.target.value})}
+                        />
                     </div>
-                </form>
-                <div className="d-flex flex-row">
+                    <div className="d-flex flex-row">
                     <div className="button-padding">
-                        <Button label="Done" className="done-button"/>
+                        <input type="submit" label="Done" className="btn btn-dark done-button"/>
                     </div>
                     <div className="button-padding">
                         <Link to="/">
@@ -86,6 +118,7 @@ class addSubscription extends Component{
                         </Link>
                     </div>
                 </div>
+                </form>
             </div>
         )
     }
